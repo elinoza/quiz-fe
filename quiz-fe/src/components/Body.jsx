ba28fd71bab30498439c055b89725ed1d540fa79
elinoza <hillcakmak@gmail.com>
2021-01-08 16:17:27 +0300

    start is working

--- quiz-fe/src/components/Body.jsx
+++ quiz-fe/src/components/Body.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Container,Button, Card, Row, Col} from "react-bootstrap"
+import {Container,Button, Form, Row, Col} from "react-bootstrap"
 
 
 // -in the first page there should be name and start buttton 
@@ -11,8 +11,76 @@ import {Container,Button, Card, Row, Col} from "react-bootstrap"
 class Body extends React.Component{
 
     state={
-        start:true,
+        start:false,
+        examInfo:{
+           
+        },
+        body : {
+            candidateName:"" },
+        loading:false,
+        errMessage:''
+        
     }
+    
+ 
+	updateField = (e) => {
+        let body={...this.state.body}
+        let currentid = e.currentTarget.id
+		
+		body[currentid] = e.currentTarget.value 
+		this.setState({ body:body})
+	}
+
+
+
+    fetch =async ()=>{
+       try{ 
+       // const url=process.env.REACT_APP_Url
+        const url="http://localhost:3005/exams"
+        let response= await fetch(`${url}/start`, 
+        { method: "POST",
+          body: JSON.stringify(this.state.body),
+		  headers: new Headers({
+		 "Content-Type": "application/json"})
+
+    })
+    console.log(response)
+        if (response.ok) {
+            let examInfo = await response.json()
+            
+            console.log("examInfo:", examInfo)
+            
+         return examInfo
+
+            }
+        else{
+        console.log("an error occurred")
+        let error = await response.json()
+        this.setState({
+            errMessage: error.message,
+            loading: false,
+            
+        })}
+        
+    }
+    catch(e){
+        console.log(e) // Error
+			this.setState({
+				errMessage: e.message,
+				loading: false,
+			})
+    }
+        }
+        
+        submitForm = (e) => {
+            e.preventDefault()
+            this.setState({ loading: true })
+            this.getQuestions()
+            
+            
+        }
+        getQuestions=async ()=>{ let examInfo= await this.fetch();this.setState({ examInfo:examInfo,start:true})}
+    
     render(){
         return(
             <Container className=" align-items-center justify-content-center   app mt-5  "  >
@@ -20,16 +88,22 @@ class Body extends React.Component{
                 <h6 className="d-inline"> You should answer 5 question</h6>
                 <h6 className="d-inline ml-auto"> Remaining time: 60 seconds</h6>
                 </div>
-                { this.state.start ?
+                { !this.state.start ?
                 <div className="answers  align-items-center justify-content-center  text-center my-5">
                     
                     <Row className="my-5">
 
                             <Col> 
-                            <Form>
+                            <Form onSubmit={this.submitForm}>
                             <Form.Group controlId="name">
                                 <Form.Label>Your Name & Surname </Form.Label>
-                                <Form.Control type="text" placeholder="Enter your name please" />
+                                <Form.Control 
+                                id="candidateName"
+                                type="text" 
+                                placeholder="Enter your name please" 
+                                value={this.state.body.candidateName}
+                                 onChange={this.updateField}
+                                />
                             </Form.Group>
 
                            
