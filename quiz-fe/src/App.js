9637b04a4fb9d5dddf2866f7ef4a0620c04dae66
elinoza <hillcakmak@gmail.com>
2021-01-08 19:51:49 +0300

    fghj

--- quiz-fe/src/App.js
+++ quiz-fe/src/App.js
@@ -9,7 +9,7 @@ import Body from "./components/Body"
 function App() {
   return (
     <div >
-  <Container className="main my-5 shadow">
+  <Container className="main my-1 shadow">
  
     <Row>
       <Col  >
