a934257e512b5f27a0bfdd908dd316c18ef49ab2
elinoza <hillcakmak@gmail.com>
2022-12-30 16:34:56 +0300

    refactoring

--- quiz-fe/src/components/Body.jsx
+++ quiz-fe/src/components/Body.jsx
@@ -1,240 +1,247 @@
-import React from "react"
-import {Container,Button, Form, Row, Col,Image} from "react-bootstrap"
-import { GiTrophyCup} from 'react-icons/gi'
+import React from "react";
+import { Container, Button, Form, Row, Col, Image } from "react-bootstrap";
+import { GiTrophyCup } from "react-icons/gi";
 
-
-// -in the first page there should be name and start buttton 
+// -in the first page there should be name and start buttton
 // -start fires function-fetch/post-start &retrieves - first question
 // -2nd page the should be question, answers, next button
 // -next button  className="shadow"fires function -fetch/post-answer id &retrieves - 2nd question
 // -last page there should be score page
 
-class Body extends React.Component{
+class Body extends React.Component {
+  state = {
+    questions: [],
+    start: "false",
+    examInfo: [],
+    body: {
+      candidateName: "",
+    },
+    loading: false,
+    errMessage: "",
+    answers: {
+      question: 0,
+      answer: 1,
+    },
+    score: 0,
+  };
 
-    state={
-        questions:[],
-        start:"false",
-        examInfo:[],
-        body : {
-            candidateName:"" },
-        loading:false,
-        errMessage:'',
-        answers:{
-            question:0,
-            answer:1
-        },
-        score:0
-        
-    }
-    
- 
-	updateField = (e) => {
-        let body={...this.state.body}
-        let currentid = e.currentTarget.id
-		
-		body[currentid] = e.currentTarget.value 
-		this.setState({ body:body})
-	}
+  updateField = (e) => {
+    let body = { ...this.state.body };
+    let currentid = e.currentTarget.id;
 
+    body[currentid] = e.currentTarget.value;
+    this.setState({ body: body });
+  };
 
+  fetch = async () => {
+    try {
+      // const url=process.env.REACT_APP_Url
+      const url = "http://localhost:3005/exams";
+      let response = await fetch(`${url}/start`, {
+        method: "POST",
+        body: JSON.stringify(this.state.body),
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+      }); 
+      console.log(response);
+      if (response.ok) {
+        let examInfo = await response.json();
+        let questions = examInfo.questions;
 
-    fetch =async ()=>{
-       try{ 
-       // const url=process.env.REACT_APP_Url
-        const url="http://localhost:3005/exams"
-        let response= await fetch(`${url}/start`, 
-        { method: "POST",
-          body: JSON.stringify(this.state.body),
-		  headers: new Headers({
-		 "Content-Type": "application/json"})
+        console.log("examInfo:", questions);
 
-    })
-    console.log(response)
-        if (response.ok) {
-            let examInfo = await response.json()
-            let questions= examInfo.questions
-            
-            
-            console.log("examInfo:", questions)
-            
-         return examInfo
-
-            }
-        else{
-        console.log("an error occurred")
-        let error = await response.json()
+        return examInfo;
+      } else {
+        console.log("an error occurred");
+        let error = await response.json();
         this.setState({
-            errMessage: error.message,
-            loading: false,
-            
-        })}
-        
+          errMessage: error.message,
+          loading: false,
+        });
+      }
+    } catch (e) {
+      console.log(e); // Error
+      this.setState({
+        errMessage: e.message,
+        loading: false,
+      });
     }
-    catch(e){
-        console.log(e) // Error
-			this.setState({
-				errMessage: e.message,
-				loading: false,
-			})
+  };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    this.getQuestions();
+  };
+  manageState = (examInfo) => {
+    this.setState({
+      examInfo: examInfo,
+      questions: examInfo.questions,
+      start: "true",
+    });
+  };
+  getQuestions = async () => {
+    let examInfo = await this.fetch();
+    this.manageState(examInfo);
+  };
+
+  next = async (e) => {
+    let answers = { ...this.state.answers };
+    answers.answer = parseInt(e.currentTarget.value);
+    this.setState({ answers: answers });
+  };
+
+  getResults = async () => {
+    const url = `http://localhost:3005/exams`;
+    let response = await fetch(`${url}/${this.state.examInfo._id}`);
+    let examInfo = await response.json();
+    console.log(examInfo);
+    this.setState({ score: examInfo });
+  };
+  nextQuo = async () => {
+    try {
+      // const url=process.env.REACT_APP_Url
+      const url = `http://localhost:3005/exams`;
+      let response = await fetch(`${url}/${this.state.examInfo._id}/answer`, {
+        method: "POST",
+        body: JSON.stringify(this.state.answers),
+        headers: new Headers
+        ({
+          "Content-Type": "application/json",
+        }),
+      });
+      console.log(response);
+      if (response.ok) {
+        let questionIndex = this.state.answers.question;
+        let answers = { ...this.state.answers };
+        if (questionIndex === this.state.questions.length-1) {
+          this.setState({ start: "finish" });
+          this.getResults();
+        } else {
+          questionIndex += 1;
+          answers.question = questionIndex;
+          this.setState({ answers: answers });
+        }
+      } else {
+        console.log("an error occurred");
+        let error = await response.json();
+        this.setState({
+          errMessage: error.message,
+          loading: false,
+        });
+      }
+    } catch (e) {
+      console.log(e); // Error
+      this.setState({
+        errMessage: e.message,
+        loading: false,
+      });
     }
-        }
-        
-        submitForm = (e) => {
-            e.preventDefault()
-            this.setState({ loading: true })
-            this.getQuestions()
-            
-            
-        }
-        manageState=(examInfo)=>{
-            this.setState({
-                 examInfo:examInfo,
-                 questions:examInfo.questions,
-                 start:"true"})
-        }
-        getQuestions=async ()=>{ let examInfo= await this.fetch();this.manageState(examInfo)}
+  };
 
-        next=async(e)=>{
-            let answers={...this.state.answers}
-		    answers.answer = parseInt(e.currentTarget.value) 
-		    this.setState({ answers:answers})
+  render() {
+    return (
+      <Container className=" align-items-center justify-content-center   app mt-5  ">
+        <div className=" border-bottom d-flex">
+          <h6 className="d-inline"> You should answer 5 question</h6>
+          <h6 className="d-inline ml-auto"> Remaining time: 60 seconds</h6>
+        </div>
+        {this.state.start === "false" && (
+          <div className="answers  align-items-center justify-content-center  text-center my-5">
+            <Row className="my-5">
+              <Col>
+                <Form onSubmit={this.submitForm}>
+                  <Form.Group controlId="name">
+                    <Form.Label>Your Name & Surname </Form.Label>
+                    <Form.Control
+                      id="candidateName"
+                      type="text"
+                      placeholder="Enter your name please"
+                      value={this.state.body.candidateName}
+                      onChange={this.updateField}
+                    />
+                  </Form.Group>
 
-
-       }
-
-       getResults= async()=>{
-        const url=`http://localhost:3005/exams`
-        let response= await fetch(`${url}/${this.state.examInfo._id}`)
-        let examInfo=await response.json()
-        console.log(examInfo)
-        this.setState({score:examInfo})
-       }
-        nextQuo=async ()=>{
-            try{ 
-                // const url=process.env.REACT_APP_Url
-                 const url=`http://localhost:3005/exams`
-                 let response= await fetch(`${url}/${this.state.examInfo._id}/answer`, 
-                 { method: "POST",
-                   body: JSON.stringify(this.state.answers),
-                   headers: new Headers({
-                  "Content-Type": "application/json"})
-         
-             })
-             console.log(response)
-                 if (response.ok) {
-                     let questionIndex=this.state.answers.question
-                     let answers={...this.state.answers}
-                     if (questionIndex===4)
-                     {
-                         this.setState({ start:"finish"})
-                         this.getResults()
-                       }
-                        
-                      else{questionIndex+=1
-                        answers.question = questionIndex
-                      this.setState({ answers:answers })
-                        }
-                     
+                  <Button className="shadow" type="submit">
+                    START
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+        )}
+        {this.state.start === "true" && (
+          <div>
+            <div className="question my-5">
+              <h5>{this.state.questions[this.state.answers.question].text}</h5>
+              {/* {!this.state.questions[this.state.answers.question].img === null && <Image src="holder.js/171x180" rounded />} */}
+            </div>
+            <div className="answers  align-items-center justify-content-center  text-center my-5 ">
+              <Row className="my-5">
+                <Col xs={12} md={6}>
+                  <Button value="0" onClick={this.next} className="shadow">
+                    {
+                      this.state.questions[this.state.answers.question]
+                        .answers[0].text
                     }
-                 else{
-                 console.log("an error occurred")
-                 let error = await response.json()
-                 this.setState({
-                     errMessage: error.message,
-                     loading: false,
-                     
-                 })
-                }
-                 
-             }
-             catch(e){
-                 console.log(e) // Error
-                     this.setState({
-                         errMessage: e.message,
-                         loading: false,
-                     })
-             }
+                  </Button>{" "}
+                </Col>
+                <Col xs={12} md={6}>
+                  <Button value="1" onClick={this.next} className="shadow">
+                    {
+                      this.state.questions[this.state.answers.question]
+                        .answers[1].text
+                    }
+                  </Button>{" "}
+                </Col>
+                <Col xs={12} md={6}>
+                  <Button value="2" onClick={this.next} className="shadow">
+                    {
+                      this.state.questions[this.state.answers.question]
+                        .answers[2].text
+                    }
+                  </Button>{" "}
+                </Col>
+                <Col xs={12} md={6}>
+                  <Button value="3" onClick={this.next} className="shadow">
+                    {
+                      this.state.questions[this.state.answers.question]
+                        .answers[3].text
+                    }
+                  </Button>{" "}
+                </Col>
+              </Row>
+            </div>
+            <Button onClick={this.nextQuo} className="shadow">
+              NEXT{" "}
+            </Button>{" "}
+          </div>
+        )}
+        {this.state.start === "finish" && (
+          <>
+            <div className=" d-flex flex-flow-column align-items-center justify-content-center  text-center my-5 finish">
+              <h1 className="d-block">FINISHED</h1>
 
-        }
-    
-    render(){
-        return(
-            <Container className=" align-items-center justify-content-center   app mt-5  "  >
-                <div className=" border-bottom d-flex">
-                <h6 className="d-inline"> You should answer 5 question</h6>
-                <h6 className="d-inline ml-auto"> Remaining time: 60 seconds</h6>
-                </div>
-                { this.state.start=== "false" &&
-                <div className="answers  align-items-center justify-content-center  text-center my-5">
-                    
-                    <Row className="my-5">
+              <div>
+                <GiTrophyCup
+                  style={{
+                    fontSize: "100px",
+                    color: "#DD6E42",
+                    textShadow: "20px 20px 50px yellow",
+                  }}
+                />
+              </div>
 
-                            <Col> 
-                            <Form onSubmit={this.submitForm}>
-                            <Form.Group controlId="name">
-                                <Form.Label>Your Name & Surname </Form.Label>
-                                <Form.Control 
-                                id="candidateName"
-                                type="text" 
-                                placeholder="Enter your name please" 
-                                value={this.state.body.candidateName}
-                                 onChange={this.updateField}
-                                />
-                            </Form.Group>
-
-                           
-                            <Button  className="shadow" type="submit">
-                               START
-                            </Button>
-                            </Form></Col>
-                            
-                        </Row>
-                    
-                    </div> 
-                } 
-                { this.state.start === "true" &&
-                    <div>
-                        <div className="question my-5">
-                            
-                        <h5>{this.state.questions[this.state.answers.question].text}</h5>
-                        {/* {!this.state.questions[this.state.answers.question].img === null && <Image src="holder.js/171x180" rounded />} */}
-                        
-                        </div>
-                    
-                    <div className="answers  align-items-center justify-content-center  text-center my-5 ">
-                        <Row className="my-5">
-                            <Col xs={12} md={6}><Button value="0" onClick={this.next} className="shadow" >{this.state.questions[this.state.answers.question].answers[0].text}</Button>{' '}</Col>
-                            <Col xs={12} md={6}><Button value="1" onClick={this.next} className="shadow">{this.state.questions[this.state.answers.question].answers[1].text}</Button>{' '}</Col>
-                            <Col xs={12} md={6}><Button  value="2" onClick={this.next} className="shadow">{this.state.questions[this.state.answers.question].answers[2].text}</Button>{' '}</Col>
-                            <Col xs={12} md={6}><Button  value="3" onClick={this.next}  className="shadow">{this.state.questions[this.state.answers.question].answers[3].text}</Button>{' '}</Col>
-                        </Row>
-                        
-                        </div>
-                        <Button  onClick={this.nextQuo}className="shadow">NEXT </Button>{' '}
-                </div>
-                }
-                {this.state.start=== "finish" && 
-                <>
-                <div className=" d-flex flex-flow-column align-items-center justify-content-center  text-center my-5 finish">
-                  
-                    <h1 className="d-block">FINISHED</h1>
-                    
-                        <div><GiTrophyCup style={{ fontSize:"100px", color:"#DD6E42",textShadow: "20px 20px 50px yellow"}}/></div>
-                     
-                    
-                    <h3 >YOUR SCORE </h3>
-                    
-                </div>
-                <div className="d-flex flex-flow-column align-items-center justify-content-center  text-center my-5 finish"><h1> {this.state.score.totalScore}</h1></div>
-                </>
-                }
-
-                
-            </Container>
-
-
-        )
-    }
-    
+              <h3>YOUR SCORE </h3>
+            </div>
+            <div className="d-flex flex-flow-column align-items-center justify-content-center  text-center my-5 finish">
+              <h1> {this.state.score.totalScore}</h1>
+            </div>
+          </>
+        )}
+      </Container>
+    );
+  }
 }
-export default Body
+export default Body;
