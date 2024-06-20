dc8ec004fae3c1a3162d12aeec7b0437c40ab936
elinoza <hillcakmak@gmail.com>
2024-06-20 16:34:36 +0300

    title added

--- quiz-fe/src/App.js
+++ quiz-fe/src/App.js
@@ -1,24 +1,14 @@
-import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./css/quiz.css"
-import {Container, Row, Col} from "react-bootstrap"
-import NavBar from "./components/NavBar"
-import Body from "./components/Body"
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/quiz.css";
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div >
-  <>
-
-  
-    
-      <Body />
-     
- 
-        
-      
-    </>
+    <div>
+      <>
+        <Body />
+      </>
     </div>
   );
 }
